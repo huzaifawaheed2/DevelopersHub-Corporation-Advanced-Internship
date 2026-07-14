9cc05e209f27ce878c30bed4821bc75b63b9dcf1
Muhammad Huzaifa Waheed <huzaifawaheed1258@gmail.com>
2026-07-14 10:53:34 +0500

    Update app.py File Code

--- Project-03-Interactive-Business-Dashboard/app/app.py
+++ Project-03-Interactive-Business-Dashboard/app/app.py
@@ -5,6 +5,12 @@ import streamlit as st
 from config import page_config
 from data_loader import load_data
 from filters import sidebar_filters
+from metrics import show_metrics
+from charts import (
+    sales_by_category_chart,
+    profit_by_category_chart,
+    sales_by_region_chart
+)
 
 # Page Configuration
 page_config()
@@ -28,6 +34,28 @@ customer behavior, regional performance, and business insights.
 )
 
 st.divider()
+
+# KPI Cards
+show_metrics(filtered_df)
+
+st.divider()
+
+# Row 1
+# Sales and Profit Charts
+col1, col2 = st.columns(2)
+
+with col1:
+    sales_by_category_chart(filtered_df)
+
+with col2:
+    profit_by_category_chart(filtered_df)
+    
+st.divider()
+
+# Row 2
+# Sales by Region Chart
+
+sales_by_region_chart(filtered_df)
 
 # Dataset Information
 

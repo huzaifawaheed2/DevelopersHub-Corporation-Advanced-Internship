075ef85ba1265185f967b45bb41d5722561b7cc2
Muhammad Huzaifa Waheed <huzaifawaheed1258@gmail.com>
2026-07-14 10:54:12 +0500

    Update data_loader.py File Code

--- Project-03-Interactive-Business-Dashboard/app/data_loader.py
+++ Project-03-Interactive-Business-Dashboard/app/data_loader.py
@@ -1,16 +1,26 @@
 from pathlib import Path
+
 import pandas as pd
 import streamlit as st
+
 
 @st.cache_data
 def load_data():
     """
-    Load the cleaned Global Superstore dataset.
+    Load cleaned Global Superstore dataset.
     """
-    
-    project_root = Path(__file__).parent.parent
-    dataset_path = project_root / "dataset" / "Global_Superstore_Cleaned.csv"
 
-    df=pd.read_csv(dataset_path, parse_dates=["Order Date"])
+    project_root = Path(__file__).resolve().parent.parent
+
+    dataset_path = (
+        project_root
+        / "dataset"
+        / "Global_Superstore_Cleaned.csv"
+    )
+
+    df = pd.read_csv(
+        dataset_path,
+        parse_dates=["Order Date"]
+    )
 
     return df
